7df26e3c5d068c6188b998f9498b22e396da943e
Rodrigo Barros <contato.rodrigobarros@gmail.com>
2023-10-23 23:06:31 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,18 @@
+/* <script>
+        document.getElementById("cadastrar").addEventListener("click", function () {
+            var nome = document.getElementById("nome").value;
+            var telefone = document.getElementById("telefone").value;
+
+            // Cria uma nova linha na tabela
+            var table = document.getElementById("contatos-table");
+            var newRow = table.insertRow(table.rows.length);
+            var cell1 = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+            cell1.innerHTML = nome;
+            cell2.innerHTML = telefone;
+
+            // Limpa os campos do formulário
+            document.getElementById("nome").value = "";
+            document.getElementById("telefone").value = "";
+        });
+    </script> */
